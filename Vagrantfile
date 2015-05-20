039a73eb49591f95e14b6c2d58016261b60ed69e
Edgar Zamora Gómez <ezamora@AST-edgar.recerca.intranet.urv.es>
2015-05-20 17:14:13 +0200

    Prepared the virtual environment to start the project

---
+++ Vagrantfile
@@ -0,0 +1,24 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/trusty32"
+  config.vm.hostname = "sds-controller-vm"
+
+  config.vm.network "forwarded_port", guest: 5432, host: 15432 # Postgresql
+
+  config.vm.synced_folder ".", "/home/vagrant/src"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = false
+    vb.memory = "1024"
+  end
+
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = "puppet/manifests"
+    puppet.module_path = "puppet/modules"
+  end
+
+  config.vm.provision "shell", path: "scripts/vagrant-init.sh"
+
+end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,47 @@
+stage { 'first':
+    before => Stage['main'];
+}
+
+class prepare {
+    class { 'apt':
+      update => {
+        frequency => 'always',
+      },
+    }
+    class { 'python' :
+      version    => 'system',
+      pip        => true,
+      dev        => true,
+      virtualenv => true,
+      gunicorn   => true,
+    }
+}
+class { 'prepare':
+    stage => first;
+}
+
+include prepare
+
+$sysPackages = ['git', 'curl', 'graphviz', 'tree', 'nodejs']
+package { $sysPackages:
+  ensure => "installed",
+  require  => Class['prepare']
+}
+
+
+class { 'postgresql::server':
+    ip_mask_allow_all_users    => '0.0.0.0/0',
+    listen_addresses           => '*',
+    postgres_password          => 'postgres',
+}
+
+postgresql::server::db { 'sds_controller':
+  user     => 'sds_controller_user',
+  password => postgresql_password('sds_controller_user', 'sds_controller_pass');
+}
+
+package {
+    "django":
+        ensure => "1.8.1",
+        provider => pip;
+}
