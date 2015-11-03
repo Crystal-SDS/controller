15d9ab4bb2529dad8abd218dbe393965d663224a
edgarzamora <cesk002@gmail.com>
2015-11-03 13:09:58 +0100

    Update version of DSL

--- DSL/metric.py
+++ DSL/metric.py
@@ -3,28 +3,37 @@ import logging
 import json
 from pyactive.controller import init_host, serve_forever, start_controller, interval, sleep
 from pyactive.exception import TimeoutError, PyactiveError
-class Metric():
+
+class Metric(object):
 
     def __init__(self):
         self._observers = {}
         self.value = None
         self.name = None
-        # host should be obtained from config file like queue names
-        # self.channel = pika.BlockingConnection(pika.ConnectionParameters(
-        #     host='localhost', port=25672)).channel()
 
     def attach(self, observer):
-        print "observer", observer.tenant
-        if not observer.tenant in self._observers.keys():
-            self._observers[observer.tenant] = set()
-        if not observer in self._observers[observer.tenant]:
-            self._observers[observer.tenant].add(observer)
+        tenant = observer.get_tenant()
+        if not tenant in self._observers.keys():
+            self._observers[tenant] = set()
+        if not observer in self._observers[tenant]:
+            self._observers[tenant].add(observer)
 
     def detach(self, observer):
+        tenant = observer.get_tenant()
         try:
-            self._observers[observer.tenant].remove(observer)
+            self._observers[tenant].remove(observer)
         except KeyError:
             pass
+
+    def init_consum(self):
+        self.consumer = self.host.spawn_id(self.id + "_consumer", "metric", "Consumer", ["localhost", 25672, self.queue, self.proxy])
+        self.start_consuming()
+
+    def start_consuming(self):
+        self.consumer.start_consuming()
+
+    def stop_consuming(self):
+        self.consumer.stop_consuming()
 
     def notify(self, body):
         data = json.loads(body)
@@ -38,22 +47,17 @@ class Metric():
 
 class Througput(Metric):
     _sync = {}
-    _async = ['get_value', 'attach', 'detach']
-    _ref = []
+    _async = ['get_value', 'attach', 'detach', 'notify', 'start_consuming','stop_consuming', 'init_consum']
+    _ref = ['attach', 'detach']
     _parallel = []
 
-    def __init__(self, queue):
+    def __init__(self, queue, host):
         Metric.__init__(self)
         # Queue name should be take from config file
-        self.channel = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost', port=25672)).channel()
-        self.channel.queue_declare(queue=queue)
-        self.channel.basic_consume(self.callback,
-                              queue=queue,
-                              no_ack=True)
-        self.thread = Thread(target=self.start_consuming)
-        self.thread.start()
+        self.host = host
 
+        self.queue = queue
+        print 'Througput initialized'
         self.name = "througput"
         # Consumer("localhost", 25672, queue, self)
         # thread1 = Consumer("localhost", 25672, queue, self)
@@ -62,106 +66,59 @@ class Througput(Metric):
 
     def get_value(self):
         return self.value
-
-    def start_consuming(self):
-        self.channel.start_consuming()
-
-    def stop_consuming(self):
-        self.channel.stop_consuming()
-        self.channel.close()
-
-    def callback(self, ch, method, properties, body):
-        print 'body', body
-        self.notify(body)
+    #
+    # def callback(self, ch, method, properties, body):
+    #     print 'body', body
+    #     self.notify(body)
 
 class Slowdown(Metric):
     _sync = {}
-    _async = ['get_value', 'attach', 'detach']
-    _ref = []
+    _async = ['get_value', 'attach', 'detach', 'notify', 'start_consuming','stop_consuming', 'init_consum']
+    _ref = ['attach', 'detach']
     _parallel = []
 
-    def __init__(self, queue):
+    def __init__(self, queue, host):
         Metric.__init__(self)
-        # Queue name should be take from config file
-        self.channel = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost', port=25672)).channel()
-        self.channel.queue_declare(queue=queue)
-        self.channel.basic_consume(self.callback,
-                              queue=queue,
-                              no_ack=True)
-        self.thread = Thread(target=self.start_consuming)
-        self.thread.start()
 
+        self.host = host
+        self.queue = queue
         self.name = "slowdown"
         # Consumer("localhost", 25672, queue, self)
         # thread1 = Consumer("localhost", 25672, queue, self)
         # Start new Threads
         # thread1.start()
+        print 'Slowdown initialized'
 
     def get_value(self):
         return self.value
 
-    def start_consuming(self):
-        self.channel.start_consuming()
+    # def callback(self, ch, method, properties, body):
+    #     print 'body', body
+    #     self.notify(body)
 
-    def stop_consuming(self):
-        self.channel.stop_consuming()
-        self.channel.close()
+class Consumer(object):
+    _sync = {}
+    _async = ['start_consuming', 'stop_consuming']
+    _ref = []
+    _parallel = []
 
-    def callback(self, ch, method, properties, body):
-        print 'body', body
-        self.notify(body)
-
-class Consumer (Thread):
     def __init__(self, host, port, queue, obj):
-        threading.Thread.__init__(self)
-
-        self._consumer_tag
         self._channel = pika.BlockingConnection(pika.ConnectionParameters(
             host='localhost', port=25672)).channel()
         self.obj = obj
         self.queue = queue
         self._channel.queue_declare(queue=queue)
-        # self.consumer = self.channel.basic_consume(self.callback,
-        #                       queue=queue,
-        #                       no_ack=True)
-
-    def run(self):
-        self.start_consume()
-
+        self.consumer = self._channel.basic_consume(self.callback,
+                                        queue=queue,
+                                        no_ack=True)
+        print 'consumer initialized'
+    def callback(self, ch, method, properties, body):
+        self.obj.notify(body)
 
     def start_consuming(self):
-        """This method sets up the consumer by first calling
-        add_on_cancel_callback so that the object is notified if RabbitMQ
-        cancels the consumer. It then issues the Basic.Consume RPC command
-        which returns the consumer tag that is used to uniquely identify the
-        consumer with RabbitMQ. We keep the value to use it when we want to
-        cancel consuming. The on_message method is passed in as a callback pika
-        will invoke when a message is fully received.
-        """
-
-        self.add_on_cancel_callback()
-        self._consumer_tag = self._channel.basic_consume(self.callback,
-                              queue=self.queue,
-                              no_ack=True)
+        print 'start to consume!!! :D'
+        self._channel.start_consuming()
 
     def stop_consuming(self):
-        """Tell RabbitMQ that you would like to stop consuming by sending the
-        Basic.Cancel RPC command.
-
-        """
-        if self._channel:
-            LOGGER.info('Sending a Basic.Cancel RPC command to RabbitMQ')
-            self._channel.basic_cancel(self.on_cancelok, self._consumer_tag)
-
-    def close_channel(self):
-        """Call to close the channel with RabbitMQ cleanly by issuing the
-        Channel.Close RPC command.
-
-        """
-        LOGGER.info('Closing the channel')
+        self._channel.stop_consuming()
         self._channel.close()
-
-    def callback(self, ch, method, properties, body):
-        print 'body', body
-        self.obj.notify(body)
--- DSL/rules_parse.py
+++ DSL/rules_parse.py
@@ -14,31 +14,45 @@ FOR Tenant WHEN"+ condition_list +"DO"+action
 
 FOR Tenant WHEN"+ condition AND condition AND condition OR condition etc.+"DO"+action
 
-TODO: Parse = TRUE or = False or condicion number. Check to convert to float or convert to boolean. 
+TODO: Parse = TRUE or = False or condicion number. Check to convert to float or convert to boolean.
 """
 
 def parse(input_string):
-# condition_list = Forward()
+# tenant_list = Forward()
+    action = oneOf("SET DELETE")
+    param = Word(alphanums)+ Suppress(Literal("=")) + Word(alphanums)
     services = "slowdown througput"
     services_options = oneOf(services)
     operand =  oneOf("< > == != <= >=")
     number = Regex(r"[+-]?\d+(:?\.\d*)?(:?[eE][+-]?\d+)?")
     word = Word(alphas)
     action = word
+    sfilter = Word(alphanums)
     when = Suppress(Literal("WHEN"))
+    with_params = Suppress(Literal("WITH"))
     do = Suppress(Literal("DO"))
     for_tenant = Suppress(Literal("FOR"))
     tenant_id = Word(alphanums)
-    limit_value = number
-    condition = Group(services_options("services_options") + operand("operand") + limit_value("limit_value"))
+    condition = Group(services_options("services_options") + operand("operand") + number("limit_value"))
     boolean_condition = oneOf("AND OR")
-
+    tenant_list = tenant_id + ZeroOrMore(Suppress("AND")+tenant_id)
+    params_list = delimitedList(param)
+    # tenant_list << (tenant_id ^ (tenant_id + "AND" + OneOrMore(tenant_list)))
     # condition_list << ( condition ^ ( condition + boolean_condition + OneOrMore(condition_list)) )
     condition_list = operatorPrecedence(condition,[
                                 ("AND", 2, opAssoc.LEFT, ),
                                 ("OR", 2, opAssoc.LEFT, ),
                                 ])
-    rule_parse = for_tenant + tenant_id("tenant_id") + when + condition_list("condition_list") + do + action("action")
+    # joinTokens = lambda tokens : "".join(tokens)
+    # stripCommas = lambda tokens : tokens[0].replace("=", ",")
+    convertToDict = lambda tokens : dict(zip(*[iter(tokens)]*2))
+    # param.setParseAction(joinTokens)
+    # param.setParseAction(stripCommas)
+    params_list.setParseAction(convertToDict)
+    # map(None, *[iter(l)]*2)
+    rule_parse = for_tenant + Group(tenant_list)("tenants") + when +\
+                condition_list("condition_list") + do + Group(action("action") + \
+                sfilter("filter") + Optional(with_params + params_list("params")))("action_list")
 
     return rule_parse.parseString(input_string)
 
@@ -76,28 +90,32 @@ def parse(input_string):
     # return rule_parse.parseString(input_string)
 #
 # rules = """\
-#     FOR 2312 WHEN slowdown > 3 DO action
-#     FOR 2312 WHEN slowdown > 3 OR slowdown > 3 AND slowdown == 5 OR slowdown <= 6 DO action
-#     FOR 2312 WHEN slowdown > 3 AND slowdown > 50 DO action""".splitlines()
-#
-# # for rule in rules:
-# #     try:
-# #         stats = rule_parse.parseString(rule)
-# #     except:
-# #         print 'This rule ***'+rule+'  *** could not be parsed'
-# #     else:
-# #         print stats.asList()
-#
+#     FOR 4f0279da74ef4584a29dc72c835fe2c9 WHEN througput < 3 OR slowdown == 1 AND througput == 5 OR througput == 6 DO SET 1 WITH param1=2
+#     FOR 2312 WHEN slowdown > 3 OR slowdown > 3 AND slowdown == 5 OR slowdown <= 6 DO SET storlet WITH param1=2, param2=3
+#     FOR 2312 WHEN slowdown > 3 AND slowdown > 50 DO SET storlet""".splitlines()
+
+# for rule in rules:
+#     try:
+#         stats = rule_parse.parseString(rule)
+#     except:
+#         print 'This rule ***'+rule+'  *** could not be parsed'
+#     else:
+#         print stats.asList()
+
 # for rule in rules:
 #
 #     stats = rule_parse.parseString(rule)
-#     print stats
-#     print "WHEN %s %s %s DO %s" % (stats.services_options, stats.operand, stats.number, stats.action)
-#     print "*************"
-#
-#     print "condition", stats.condition, "condition2", stats.condition_list
-#     print "*************"
-#     print "*************"
-#     print "*************"
-#
-#     print "*************"
+#     print stats.asList()
+#     print stats.action_list.params
+
+
+    # print 'action: ', stats.action_list.action
+    # print "WHEN %s %s %s DO %s" % (stats.services_options, stats.operand, stats.number, stats.action_list)
+    # print "*************"
+    #
+    # print "condition", stats.condition, "condition2", stats.condition_list
+    # print "*************"
+    # print "*************"
+    # print "*************"
+# "FOR 4f0279da74ef4584a29dc72c835fe2c9 WHEN througput < 3 OR slowdown == 1 AND througput == 5 OR througput == 6 DO SET 1 WITH param1=2"
+    # print "*************"
