879a2d55476054d666b8488fd7cba5d045517e9d
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-04-26 17:03:35 +0200

    New API calls to manage SLAs.

--- sds_controller/bw/urls.py
+++ sds_controller/bw/urls.py
@@ -1,25 +1,25 @@
-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(r'^/?$', views.bw_list),
+    # url(r'^/?$', views.bw_list),
 
-    url(r'^/clear/$', views.bw_clear_all),
-    url(r'^/osinfo/$', views.osinfo),
-    
-    url(r'^/(?P<account>\w+)/$', views.bw_detail),
+    # url(r'^/clear/$', views.bw_clear_all),
+    # url(r'^/osinfo/$', views.osinfo),
+    #
+    # url(r'^/(?P<account>\w+)/$', views.bw_detail),
+    #
+    # url(r'^/clear/(?P<account>\w+)/$', views.bw_clear_account),
+    # url(r'^/clear/(?P<account>\w+)/(?P<policy>\w+)/$', views.bw_clear_policy),
+    #
+    # url(r'^/(?P<account>\w+)/(?P<bw_value>[0-9]+)/$', views.bw_update),
+    # url(r'^/(?P<account>\w+)/(?P<policy>\w+)/(?P<bw_value>[0-9]+)/$', views.bw_update_policy),
 
-    url(r'^/clear/(?P<account>\w+)/$', views.bw_clear_account),
-    url(r'^/clear/(?P<account>\w+)/(?P<policy>\w+)/$', views.bw_clear_policy),
+    url(r'^/slas/?$', views.bw_list),
+    url(r'^/sla/(?P<id>[0-9]+)/?$', views.bw_detail),
 
-    url(r'^/(?P<account>\w+)/(?P<bw_value>[0-9]+)/$', views.bw_update),
-    url(r'^/(?P<account>\w+)/(?P<policy>\w+)/(?P<bw_value>[0-9]+)/$', views.bw_update_policy),
-
-
-
-    #Not implemented
-    #url(r'^/osinfo/(?P<ip>)$', views.bw_delete),
+    # Not implemented
+    # url(r'^/osinfo/(?P<ip>)$', views.bw_delete),
 
 ]
--- sds_controller/bw/views.py
+++ sds_controller/bw/views.py
@@ -1,14 +1,14 @@
 import requests
-import ConfigParser
-try:
-    import simplejson as json
-except ImportError:
-    import json
+
+
+import json
 from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser, FileUploadParser
+import redis
+
 
 # Create your views here.
 
@@ -16,16 +16,19 @@ class JSONResponse(HttpResponse):
     """
     An HttpResponse that renders its content into JSON.
     """
+
     def __init__(self, data, **kwargs):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
+
 def proxyaddress():
     """
     Reads the proxy address from django settings.
     """
-    return settings.SWIFT_URL+settings.SWIFT_API_VERSION+"/"
+    return settings.SWIFT_URL + "/"
+
 
 def is_valid_request(request):
     headers = {}
@@ -35,124 +38,213 @@ def is_valid_request(request):
     except:
         return None
 
+
+def get_redis_connection():
+    return redis.Redis(connection_pool=settings.REDIS_CON_POOL)
+
+
+# @csrf_exempt
+# def bw_list_redis(request):
+#     """
+#     Ask the redis database information about the assigned BW to each
+#     account and policy.
+#     """
+#     try:
+#         r = get_redis_connection()
+#     except:
+#         return JSONResponse('Error connecting with DB', status=500)
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself with the header X-Auth-Token ', status=401)
+#     if request.method == 'GET':
+#         keys = r.keys("bw:*")
+#         bwdict = dict()
+#         for key in keys:
+#             bw = r.hgetall(key)
+#             bwdict[key[3:]] = bw
+#         return JSONResponse(bwdict, status=200)
+#     return JSONResponse('Only HTTP GET /bw/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def bw_detail_redis(request, account):
+#     """
+#     Ask the information of a certain tenant to redis.
+#     """
+#     try:
+#         r = get_redis_connection()
+#     except:
+#         return JSONResponse('Error connecting with DB', status=500)
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'GET':
+#         bwdict = dict()
+#         bwdict[account] = r.hgetall('bw:' + account)
+#         return JSONResponse(bwdict, status=200)
+#     return JSONResponse('Only HTTP GET /bw/<account>/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def bw_clear_all(request):
+#     """
+#     This call clears all the BW assignations for all accounts and policies.
+#     """
+#     try:
+#         r = get_redis_connection()
+#     except:
+#         return JSONResponse('Error connecting with DB', status=500)
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'PUT':
+#         keys = r.keys("bw:*")
+#         for key in keys:
+#             r.delete(key)
+#         return HttpResponse(request, status=200)
+#     return JSONResponse('Only HTTP PUT /bw/clear/<account>/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def bw_clear_account(request, account):
+#     """
+#     This call clears all the BW assignations entries for the selected account.
+#     """
+#     try:
+#         r = get_redis_connection()
+#     except:
+#         return JSONResponse('Error connecting with DB', status=500)
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'PUT':
+#         r.delete('bw:' + account)
+#         return HttpResponse(request, status=200)
+#     return JSONResponse('Only HTTP PUT /bw/clear/<account>/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def bw_clear_policy(request, account, policy):
+#     """
+#     This call clears all the BW assignations entries for the selected account
+#     and policy.
+#     """
+#     try:
+#         r = get_redis_connection()
+#     except:
+#         return JSONResponse('Error connecting with DB', status=500)
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'PUT':
+#         r.hdel('bw:' + account, policy)
+#         return HttpResponse(request, status=200)
+#     return JSONResponse('Only HTTP PUT /bw/clear/<account>/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def bw_update(request, account, bw_value):
+#     '''
+#     This call assigns the specified bw to all the policies of the selected
+#     account
+#     '''
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'PUT':
+#         address = proxyaddress() + "bwmod/" + account + "/" + bw_value + "/"
+#         r = requests.get(address, headers=headers)
+#         return HttpResponse(r.content, content_type='application/json', status=r.status_code)
+#     return JSONResponse('Only HTTP PUT /bw/<account>/<bw_value>/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def bw_update_policy(request, account, policy, bw_value):
+#     '''
+#     This call assigns the specified bw to all the policies of the selected
+#     account
+#     '''
+#     try:
+#         r = get_redis_connection()
+#     except:
+#         return JSONResponse('Error connecting with DB', status=500)
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'PUT':
+#         r.hset('bw:' + account, policy, bw_value)
+#         return HttpResponse(request, status=200)
+#     return JSONResponse('Only HTTP PUT /bw/clear/<account>/ requests allowed.', status=405)
+#
+#
+# @csrf_exempt
+# def osinfo(request):
+#     """
+#     Ask the proxy server information about the current objects and its BW.
+#     """
+#     headers = is_valid_request(request)
+#     if not headers:
+#         return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+#     if request.method == 'GET':
+#         address = proxyaddress() + "osinfo/"
+#         r = requests.get(address, headers=headers)
+#         return HttpResponse(r.content, content_type='application/json', status=r.status_code)
+#     return JSONResponse('Only HTTP GET /bw/osinfo/ requests allowed.', status=405)
+
+############################################################################################
+
 @csrf_exempt
 def bw_list(request):
     """
-    Ask the proxy server information about the assigned BW to each
-    account and policy.
+    List all slas, or create a SLA.
     """
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+    try:
+        r = get_redis_connection()
+    except:
+        return JSONResponse('Error connecting with DB', status=500)
+
     if request.method == 'GET':
-        address = proxyaddress() + "/bwdict/"
-        r = requests.get(address, headers=headers)
-        return JSONResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP GET /bw/ requests allowed.', status=405)
+        keys = r.keys("bw:*")
+        dependencies = []
+        for key in keys:
+            dependencies.append(r.hgetall(key))
+        return JSONResponse(dependencies, status=200)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        dependency_id = r.incr("slas:id")
+        try:
+            data["id"] = dependency_id
+            r.hmset('bw:' + str(dependency_id), data)
+            return JSONResponse(data, status=201)
+        except:
+            return JSONResponse("Error saving SLA.", status=400)
+    return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
+
 
 @csrf_exempt
-def bw_detail(request, account):
+def bw_detail(request, id):
     """
-    Ask the information of a certain tenant.
+    Retrieve, update or delete SLA.
     """
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+    try:
+        r = get_redis_connection()
+    except:
+        return JSONResponse('Error connecting with DB', status=500)
+
     if request.method == 'GET':
-        dict_json = dict()
-        address = proxyaddress() + "/bwdict/"
-        r = requests.get(address)
-        data = json.loads(r.content, headers=headers)
-        for os in data:
-            dict_json[os] = { k : v for k,v in data[os].iteritems() if k == account}
-        return JSONResponse(json.dumps(dict_json), content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP GET /bw/<account>/ requests allowed.', status=405)
+        dependency = r.hgetall("bw:" + str(id))
+        return JSONResponse(dependency, status=200)
 
-@csrf_exempt
-def bw_clear_all(request):
-    """
-    This call clears all the BW assignations for all accounts and policies.
-    """
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
-    if request.method == 'PUT':
-        address = proxyaddress() + "/bwmod/"
-        r = requests.get(address, headers=headers)
-        return HttpResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP PUT /bw/clear/ requests allowed.', status=405)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        try:
+            r.hmset('bw:' + str(id), data)
+            return JSONResponse("Data updated", status=201)
+        except:
+            return JSONResponse("Error updating data", status=400)
 
-@csrf_exempt
-def bw_clear_account(request, account):
-    """
-    This call clears all the BW assignations entries for the selected account.
-    """
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
-    if request.method == 'PUT':
-        address = proxyaddress() + "/bwmod/" + account + "/"
-        r = requests.get(address,headers=headers)
-        return HttpResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP PUT /bw/clear/<account>/ requests allowed.', status=405)
-
-@csrf_exempt
-def bw_clear_policy(request, account, policy):
-    """
-    This call clears all the BW assignations entries for the selected account
-    and policy.
-    """
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
-    if request.method == 'PUT':
-        address = proxyaddress() + "/bwmod/" + account + "/" + policy + "/"
-        r = requests.get(address, headers=headers)
-        return HttpResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP PUT /bw/clear/<account>/<policy>/ requests allowed.', status=405)
-
-
-@csrf_exempt
-def bw_update(request, account, bw_value):
-    '''
-    This call assigns the specified bw to all the policies of the selected
-    account
-    '''
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
-    if request.method == 'PUT':
-        address = proxyaddress() + "/bwmod/" + account + "/" + bw_value + "/"
-        r = requests.get(address, headers=headers)
-        return HttpResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP PUT /bw/<account>/<bw_value>/ requests allowed.', status=405)
-
-@csrf_exempt
-def bw_update_policy(request, account, policy, bw_value):
-    '''
-    This call assigns the specified bw to all the policies of the selected
-    account
-    '''
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
-    if request.method == 'PUT':
-        address = proxyaddress() + "/bwmod/" + account + "/" + policy + "/" + bw_value + "/"
-        r = requests.get(address, headers=headers)
-        return HttpResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP PUT /bw/clear/<account>/<policy>/<bw_value>/ requests allowed.', status=405)
-
-
-@csrf_exempt
-def osinfo(request):
-    """
-    Ask the proxy server information about the current objects and its BW.
-    """
-    headers = is_valid_request(request)
-    if not headers:
-        return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
-    if request.method == 'GET':
-        address = proxyaddress() + "/osinfo/"
-        r = requests.get(address, headers=headers)
-        return HttpResponse(r.content, content_type = 'application/json', status=r.status_code)
-    return JSONResponse('Only HTTP GET /bw/osinfo/ requests allowed.', status=405)
+    elif request.method == 'DELETE':
+        r.delete("bw:" + str(id))
+        return JSONResponse('SLA has been deleted', status=204)
+    return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
