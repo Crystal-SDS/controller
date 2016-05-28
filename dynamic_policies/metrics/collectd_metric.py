999420fab56dfacb7e895fb25091d97d1a8c73a4
JosepSampe <jsampe@gmail.com>
2016-05-28 16:53:29 +0200

    Threaded monitoring

--- dynamic_policies/metrics/collectd_metric.py
+++ dynamic_policies/metrics/collectd_metric.py
@@ -1,7 +1,6 @@
 from abstract_metric import Metric
 from metrics_parser import SwiftMetricsParse
-import json
-import socket
+
 
 class CollectdMetric(Metric):
     _sync = {}
@@ -17,50 +16,24 @@ class CollectdMetric(Metric):
         self.name = metric_id
         self.exchange = exchange
         self.parser_instance = SwiftMetricsParse()
-        print 'GET BW tenant initialized'
-        
-        # self.oh = open("/home/lab144/oh_"+metric_id+".dat", "w")
-        
-
 
     def notify(self, body):
         """
-        OLD SET:
         PUT VAL swift_mdw/groupingtail-swift_metrics*4f0279da74ef4584a29dc72c835fe2c9*get_bw_tenant/counter interval=5.000 1448964179.433:198
-
-        NEW SET:
-        {"0.0.0.0:8080": {"AUTH_bd34c4073b65426894545b36f0d8dcce": 3}}
         """
-        monitoring_data = dict()
-        print body
-        print '********************************************************'
-        data = json.loads(body)
+        #print 'body', body
+        #print '********************************************************'
+        body_parsed = self.parser_instance.parse(body)
+        #print 'body_parsed', body_parsed.target
+        #print 'observers', self._observers.keys()
+        #print '********************************************************'
         
-        try:
-            tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            logstah_server_address = ("iostack.urv.cat", 5400)
-            tcpsock.connect(logstah_server_address)
-
-            for source_ip in data:
-                monitoring_data['metric_name'] = self.queue
-                monitoring_data['source_ip'] = source_ip.replace('.','-')
-                for key, value in data[source_ip].items():
-                    monitoring_data['metric_target'] = key.replace('AUTH_', '')
-                    monitoring_data['value'] = value
-                    tcpsock.sendall(json.dumps(monitoring_data)+'\n')
-                monitoring_data = dict()
-            tcpsock.close()
-        except:
-            pass
-        
-        """
         try:
             for observer in self._observers[body_parsed.target]:
                 observer.update(self.name, body_parsed)
         except:
             #print "fail", body_parsed
             pass
-        """
 
     def get_value(self):
         return self.value
---
+++ dynamic_policies/metrics/swift_metric.py
@@ -0,0 +1,77 @@
+from abstract_metric import Metric
+from metrics_parser import SwiftMetricsParse
+from threading import Thread
+import datetime
+import json
+import socket
+
+
+class SwiftMetric(Metric):
+    _sync = {}
+    _async = ['get_value', 'attach', 'detach', 'notify', 'start_consuming','stop_consuming', 'init_consum', 'stop_actor']
+    _ref = ['attach', 'detach']
+    _parallel = []
+
+    def __init__(self, exchange, metric_id, routing_key):
+        Metric.__init__(self)
+
+        self.queue = metric_id
+        self.routing_key = routing_key
+        self.name = metric_id
+        self.exchange = exchange
+        self.parser_instance = SwiftMetricsParse()
+        self.logstah_server = ("iostack.urv.cat", 5400)
+        self.last_metrics = dict()
+        
+    def notify(self, body):
+        """
+        {"0.0.0.0:8080": {"AUTH_bd34c4073b65426894545b36f0d8dcce": 3}}
+        """
+        print '********************************************************'
+        print body
+        data = json.loads(body)
+
+        Thread(target=self._send_data_to_logstash,args=(data, )).start()
+
+
+        """
+        try:
+            for observer in self._observers[body_parsed.target]:
+                observer.update(self.name, body_parsed)
+        except:
+            #print "fail", body_parsed
+            pass
+        """
+
+    def get_value(self):
+        return self.value
+    
+    def _send_data_to_logstash(self, data):
+        monitoring_data = dict()
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            for source_ip in data:
+                monitoring_data['metric_name'] = self.queue
+                monitoring_data['source_ip'] = source_ip.replace('.','-')
+                for key, value in data[source_ip].items():
+                    monitoring_data['metric_target'] = key.replace('AUTH_', '')
+  
+                    if (key in self.last_metrics and self.last_metrics[key]['value'] == 0) or key not in self.last_metrics:
+                        monitoring_data['value'] = 0
+                        date = datetime.datetime.now() - datetime.timedelta(seconds=1)
+                        monitoring_data['@timestamp'] = str(date.isoformat())
+                        message = json.dumps(monitoring_data)+'\n'    
+                        sock.sendto(message, self.logstah_server)
+                        
+                    monitoring_data['value'] = value
+                    if '@timestamp' in monitoring_data:
+                        del monitoring_data['@timestamp']
+                    message = json.dumps(monitoring_data)+'\n'    
+                    sock.sendto(message, self.logstah_server)
+                    
+                    self.last_metrics[key] = monitoring_data
+                monitoring_data = dict()
+        except Exception as inst:
+            print inst
+            print "Error sending monitoring data to logstash"
+            pass
