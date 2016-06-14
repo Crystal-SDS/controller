f014a8c57249477c22303ce438c6259d92655910
gerardparis <gerard.paris@gmail.com>
2016-06-14 12:59:43 +0200

    Configured Redis for testing

--- sds_controller/registry/tests.py
+++ sds_controller/registry/tests.py
@@ -1,11 +1,20 @@
-from django.test import TestCase, RequestFactory
+import redis
+
+from django.test import TestCase, RequestFactory, override_settings
+from django.conf import settings
 
 from .views import policy_list
 
+# Tests use database=10 instead of 0.
+@override_settings(REDIS_CON_POOL = redis.ConnectionPool(host='localhost', port=6379, db=10))
 class NewTestCase(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
+
+    def tearDown(self):
+        r = redis.Redis(connection_pool=settings.REDIS_CON_POOL)
+        r.flushdb()
 
     def test_registry_static_policy(self):
         """..."""
