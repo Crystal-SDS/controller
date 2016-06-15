c5b42381c625eae967259b3a17442895617ce47a
gerardparis <gerard.paris@gmail.com>
2016-06-15 11:45:04 +0200

    Added storlet tests

--- sds_controller/storlet/tests.py
+++ sds_controller/storlet/tests.py
@@ -4,8 +4,9 @@ import redis
 
 from django.test import TestCase, RequestFactory, override_settings
 from django.conf import settings
+from rest_framework import status
 
-from .views import storlet_list
+from .views import storlet_list, storlet_detail
 
 # Tests use database=10 instead of 0.
 @override_settings(REDIS_CON_POOL = redis.ConnectionPool(host='localhost', port=6379, db=10))
@@ -16,11 +17,10 @@ class StorletTestCase(TestCase):
 
     def tearDown(self):
         r = redis.Redis(connection_pool=settings.REDIS_CON_POOL)
-        #r.flushdb()
+        r.flushdb()
 
     def test_list_storlet(self):
-        """..."""
-
+        """Retrieve storlet list"""
         # Create a storlet
         self.create_storlet()
 
@@ -28,12 +28,64 @@ class StorletTestCase(TestCase):
         request = self.factory.get('/filters')
         response = storlet_list(request)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertNotEqual(response.content, [])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(response.content, "[]")
 
         storlets = json.loads(response.content)
 
         self.assertEqual(storlets[0]['name'], "FakeFilter")
+
+    def test_delete_storlet(self):
+        """Delete a storlet"""
+        # Create a storlet
+        self.create_storlet()
+
+        request = self.factory.delete('/filters/1')
+        response = storlet_detail(request, "1")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        request = self.factory.get('/filters')
+        response = storlet_list(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content, "[]")
+
+    def test_update_storlet(self):
+        """Update a storlet"""
+        self.create_storlet()
+
+        filter_updated_data = {
+            'name': 'FakeFilter', 'language': 'java', 'interface_version': '', 'dependencies': '',
+            'object_metadata': '', 'main': 'com.example.UpdatedFakeMain', 'is_put': 'False', 'is_get': 'False',
+            'has_reverse': 'False', 'execution_server': 'proxy', 'execution_server_reverse': 'proxy'}
+        request = self.factory.put('/filters/1', json.dumps(filter_updated_data), 'application/json')
+        response = storlet_detail(request, "1")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        request = self.factory.get('/filters')
+        response = storlet_list(request)
+        storlets = json.loads(response.content)
+        self.assertEqual(storlets[0]['main'], 'com.example.UpdatedFakeMain')
+
+    def test_create_storlet(self):
+        """Create 2 storlets"""
+        self.create_storlet()
+
+        filter_data = {'name': 'SecondFilter', 'language': 'java', 'interface_version': '', 'dependencies': '',
+                       'object_metadata': '', 'main': 'com.example.SecondMain', 'is_put': 'False', 'is_get': 'False',
+                       'has_reverse': 'False', 'execution_server': 'proxy', 'execution_server_reverse': 'proxy'}
+
+        request = self.factory.post('/filters/', json.dumps(filter_data), 'application/json')
+        response = storlet_list(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        request = self.factory.get('/filters')
+        response = storlet_list(request)
+        storlets = json.loads(response.content)
+        self.assertEqual(len(storlets), 2)
+        self.assertEqual(storlets[0]['name'], 'FakeFilter')
+        self.assertEqual(storlets[1]['name'], 'SecondFilter')
+
+
 
     #
     # Aux methods
@@ -45,4 +97,4 @@ class StorletTestCase(TestCase):
                        'has_reverse': 'False', 'execution_server': 'proxy', 'execution_server_reverse': 'proxy'}
         request = self.factory.post('/filters/', json.dumps(filter_data), 'application/json')
         response = storlet_list(request)
-        self.assertEqual(response.status_code, 201) # created
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
