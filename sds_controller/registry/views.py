9fdd0f97f984da9e7d0d8a11772c384604007c1f
gerardparis <gerard.paris@gmail.com>
2016-06-22 12:08:52 +0200

    Modified object_type registry views

--- sds_controller/registry/views.py
+++ sds_controller/registry/views.py
@@ -110,7 +110,8 @@ def metric_detail(request, name):
 
         data = JSONParser().parse(request)
         r.hmset('metric:' + str(name), data)
-        return JSONResponse('The metadata of the metric workload with name: ' + str(name) + ' has been updated', status=201)
+        return JSONResponse('The metadata of the metric workload with name: ' + str(name) + ' has been updated',
+                            status=201)
 
     if request.method == 'DELETE':
         r.delete("metric:" + str(id))
@@ -170,7 +171,8 @@ def dynamic_filter_detail(request, name):
             return JSONResponse('Dynamic filter with name:  ' + str(name) + ' not exists.', status=404)
         data = JSONParser().parse(request)
         r.hmset('filter:' + str(name), data)
-        return JSONResponse('The metadata of the dynamic filter with name: ' + str(name) + ' has been updated', status=201)
+        return JSONResponse('The metadata of the dynamic filter with name: ' + str(name) + ' has been updated',
+                            status=201)
 
     if request.method == 'DELETE':
         r.delete("filter:" + str(name))
@@ -227,7 +229,8 @@ def storage_node_detail(request, snode_id):
             return JSONResponse('Storage node with name:  ' + str(snode_id) + ' not exists.', status=404)
         data = JSONParser().parse(request)
         r.hmset('SN:' + str(snode_id), data)
-        return JSONResponse('The metadata of the storage node with name: ' + str(snode_id) + ' has been updated', status=201)
+        return JSONResponse('The metadata of the storage node with name: ' + str(snode_id) + ' has been updated',
+                            status=201)
 
     if request.method == 'DELETE':
         r.delete("SN:" + str(snode_id))
@@ -262,7 +265,7 @@ def add_tenants_group(request):
     if request.method == 'POST':
         gtenant_id = r.incr("gtenant:id")
         data = JSONParser().parse(request)
-        r.lpush('G:' + str(gtenant_id), *data)
+        r.rpush('G:' + str(gtenant_id), *data)
         return JSONResponse('Tenants group has been added in the registy', status=201)
 
     return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
@@ -285,11 +288,13 @@ def tenants_group_detail(request, gtenant_id):
 
     if request.method == 'PUT':
         if not r.exists('G:' + str(gtenant_id)):
-            return JSONResponse('The members of the tenants group with id:  ' + str(gtenant_id) + ' not exists.', status=404)
+            return JSONResponse('The members of the tenants group with id:  ' + str(gtenant_id) + ' not exists.',
+                                status=404)
         data = JSONParser().parse(request)
         # for tenant in data:
-        r.lpush('G:' + str(gtenant_id), *data)
-        return JSONResponse('The members of the tenants group with id: ' + str(gtenant_id) + ' has been updated', status=201)
+        r.rpush('G:' + str(gtenant_id), *data)
+        return JSONResponse('The members of the tenants group with id: ' + str(gtenant_id) + ' has been updated',
+                            status=201)
 
     if request.method == 'DELETE':
         r.delete("G:" + str(gtenant_id))
@@ -308,7 +313,8 @@ def gtenants_tenant_detail(request, gtenant_id, tenant_id):
         return JSONResponse('Error connecting with DB', status=500)
     if request.method == 'DELETE':
         r.lrem("G:" + str(gtenant_id), str(tenant_id), 1)
-        return JSONResponse('Tenant' + str(tenant_id) + 'has been deleted from group with the id: ' + str(gtenant_id), status=204)
+        return JSONResponse('Tenant' + str(tenant_id) + 'has been deleted from group with the id: ' + str(gtenant_id),
+                            status=204)
     return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
 
 
@@ -326,7 +332,7 @@ def object_type_list(request):
     try:
         r = get_redis_connection()
     except:
-        return JSONResponse('Error connecting with DB', status=500)
+        return JSONResponse('Error connecting with DB', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     if request.method == 'GET':
         keys = r.keys("object_type:*")
@@ -335,21 +341,23 @@ def object_type_list(request):
             name = key.split(":")[1]
             types_list = r.lrange(key, 0, -1)
             object_types.append({"name": name, "types_list": types_list})
-        return JSONResponse(object_types, status=200)
+        return JSONResponse(object_types, status=status.HTTP_200_OK)
 
     if request.method == "POST":
         data = JSONParser().parse(request)
         name = data.pop("name", None)
         if not name:
-            return JSONResponse('Object type must have a name as identifier', status=400)
-        if not "types_list" in data:
-            return JSONResponse('Object type must have a types_list defining the valid object types', status=400)
+            return JSONResponse('Object type must have a name as identifier', status=status.HTTP_400_BAD_REQUEST)
+        if r.exists('object_type:' + str(name)):
+            return JSONResponse('Object type ' + str(name) + ' already exists.', status=status.HTTP_400_BAD_REQUEST)
+        if "types_list" not in data:
+            return JSONResponse('Object type must have a types_list defining the valid object types', status=status.HTTP_400_BAD_REQUEST)
 
-        if r.lpush('object_type:' + str(name), *data["types_list"]):
-            return JSONResponse('Object type has been added in the registy', status=201)
-        return JSONResponse('Error storing the object type in the DB', status=500)
+        if r.rpush('object_type:' + str(name), *data["types_list"]):
+            return JSONResponse('Object type has been added in the registy', status=status.HTTP_201_CREATED)
+        return JSONResponse('Error storing the object type in the DB', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
+    return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 @csrf_exempt
@@ -364,23 +372,27 @@ def object_type_detail(request, object_type_name):
     except:
         return JSONResponse('Error connecting with DB', status=500)
 
+    key = "object_type:" + object_type_name
     if request.method == 'GET':
-        if r.exists("object_type:" + object_type_name):
-            object_type = r.lrange("object_type:" + object_type_name, 0, -1)
+        if r.exists(key):
+            types_list = r.lrange(key, 0, -1)
+            object_type = {"name": object_type_name, "types_list": types_list}
             return JSONResponse(object_type, status=200)
         return JSONResponse("Object type not found", status=404)
 
     if request.method == "PUT":
-        if not r.exists("object_type:" + object_type_name):
-            return JSONResponse('The object type with name:  ' + object_type_name + ' does not exist.', status=404)
+        if not r.exists(key):
+            return JSONResponse('The object type with name: ' + object_type_name + ' does not exist.', status=404)
         data = JSONParser().parse(request)
-        if r.lpush("object_type:" + object_type_name, *data):
+        pipe = r.pipeline()
+        # the following commands are buffered in a single atomic request (to replace current contents)
+        if pipe.delete(key).rpush(key, *data).execute():
             return JSONResponse('The object type ' + str(object_type_name) + ' has been updated', status=201)
         return JSONResponse('Error storing the object type in the DB', status=500)
 
     if request.method == "DELETE":
-        if r.exists("object_type:" + object_type_name):
-            object_type = r.delete("object_type:" + object_type_name)
+        if r.exists(key):
+            object_type = r.delete(key)
             return JSONResponse(object_type, status=200)
         return JSONResponse("Object type not found", status=404)
     return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
@@ -399,7 +411,8 @@ def object_type_items_detail(request, object_type_name, item_name):
         return JSONResponse('Error connecting with DB', status=500)
     if request.method == 'DELETE':
         r.lrem("object_type:" + str(object_type_name), str(item_name), 1)
-        return JSONResponse('Tenant' + str(tenant_id) + 'has been deleted from group with the id: ' + str(gtenant_id), status=204)
+        return JSONResponse('Tenant' + str(tenant_id) + 'has been deleted from group with the id: ' + str(gtenant_id),
+                            status=204)
     return JSONResponse('Method ' + str(request.method) + ' not allowed.', status=405)
 
 
@@ -417,7 +430,9 @@ def policy_list(request):
         if 'static' in str(request.path):
             headers = is_valid_request(request)
             if not headers:
-                return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=status.HTTP_401_UNAUTHORIZED)
+                return JSONResponse(
+                    'You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ',
+                    status=status.HTTP_401_UNAUTHORIZED)
             keystone_response = requests.get(settings.KEYSTONE_URL + "tenants", headers=headers)
             keystone_tenants = json.loads(keystone_response.content)['tenants']
 
@@ -430,7 +445,13 @@ def policy_list(request):
             for it in keys:
                 for key, value in r.hgetall(it).items():
                     json_value = json.loads(value)
-                    policies.append({'id': key, 'target_id': it.replace('pipeline:AUTH_', ''), 'target_name': tenants_list[it.replace('pipeline:AUTH_', '')], 'filter_name': json_value['filter_name'], 'object_type': json_value['object_type'], 'object_size': json_value['object_size'], 'execution_server': json_value['execution_server'], 'execution_server_reverse': json_value['execution_server_reverse'], 'execution_order': json_value['execution_order'], 'params': json_value['params']})
+                    policies.append({'id': key, 'target_id': it.replace('pipeline:AUTH_', ''),
+                                     'target_name': tenants_list[it.replace('pipeline:AUTH_', '')],
+                                     'filter_name': json_value['filter_name'], 'object_type': json_value['object_type'],
+                                     'object_size': json_value['object_size'],
+                                     'execution_server': json_value['execution_server'],
+                                     'execution_server_reverse': json_value['execution_server_reverse'],
+                                     'execution_order': json_value['execution_order'], 'params': json_value['params']})
             return JSONResponse(policies, status=status.HTTP_200_OK)
 
         elif 'dynamic' in str(request.path):
@@ -447,7 +468,9 @@ def policy_list(request):
     if request.method == 'POST':
         headers = is_valid_request(request)
         if not headers:
-            return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=status.HTTP_401_UNAUTHORIZED)
+            return JSONResponse(
+                'You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ',
+                status=status.HTTP_401_UNAUTHORIZED)
         rules_string = request.body.splitlines()
 
         for rule_string in rules_string:
@@ -473,7 +496,8 @@ def policy_list(request):
             except Exception as e:
                 print("The rule: " + rule_string + " cannot be parsed")
                 print("Exception message", e)
-                return JSONResponse("Error in rule: " + rule_string + " Error message --> " + str(e), status=status.HTTP_401_UNAUTHORIZED)
+                return JSONResponse("Error in rule: " + rule_string + " Error message --> " + str(e),
+                                    status=status.HTTP_401_UNAUTHORIZED)
 
         return JSONResponse('Policies added successfully!', status=status.HTTP_201_CREATED)
 
@@ -496,7 +520,8 @@ def static_policy_detail(request, policy_id):
     if request.method == 'GET':
         headers = is_valid_request(request)
         if not headers:
-            return JSONResponse('You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
+            return JSONResponse(
+                'You must be authenticated. You can authenticate yourself  with the header X-Auth-Token ', status=401)
         keystone_response = requests.get(settings.KEYSTONE_URL + "tenants", headers=headers)
         keystone_tenants = json.loads(keystone_response.content)["tenants"]
 
@@ -572,7 +597,8 @@ def do_action(request, r, rule_parsed, headers):
                     if rule_parsed.object_list.object_type:
                         policy_data["object_type"] = rule_parsed.object_list.object_type.object_value
                     if rule_parsed.object_list.object_size:
-                        policy_data["object_size"] = [rule_parsed.object_list.object_size.operand, rule_parsed.object_list.object_size.object_value]
+                        policy_data["object_size"] = [rule_parsed.object_list.object_size.operand,
+                                                      rule_parsed.object_list.object_size.object_value]
                 if action_info.execution_server:
                     policy_data["execution_server"] = action_info.execution_server
                 if action_info.params:
@@ -608,14 +634,18 @@ def deploy_policy(r, rule_string, parsed_rule):
 
             if action_info.transient:
                 print 'Transient rule:', parsed_rule
-                rules[cont] = remote_host.spawn_id('policy:' + str(policy_id), 'rule_transient', 'TransientRule', [rules_to_parse[key], action_info, key, remote_host])
+                rules[cont] = remote_host.spawn_id('policy:' + str(policy_id), 'rule_transient', 'TransientRule',
+                                                   [rules_to_parse[key], action_info, key, remote_host])
                 location = "/rule_transient/TransientRule/"
             else:
                 print 'Rule:', parsed_rule
-                rules[cont] = remote_host.spawn_id('policy:' + str(policy_id), 'rule', 'Rule', [rules_to_parse[key], action_info, key, remote_host])
+                rules[cont] = remote_host.spawn_id('policy:' + str(policy_id), 'rule', 'Rule',
+                                                   [rules_to_parse[key], action_info, key, remote_host])
                 location = "/rule/Rule/"
 
             rules[cont].start_rule()
             # Add policy into redis
-            r.hmset('policy:' + str(policy_id), {"id": policy_id, "policy": rule_string, "policy_description": parsed_rule, "policy_location": settings.PYACTIVE_URL + location + str(policy_id), "alive": True})
+            r.hmset('policy:' + str(policy_id),
+                    {"id": policy_id, "policy": rule_string, "policy_description": parsed_rule,
+                     "policy_location": settings.PYACTIVE_URL + location + str(policy_id), "alive": True})
             cont += 1
