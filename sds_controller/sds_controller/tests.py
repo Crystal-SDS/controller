3eaf8bdfacd3e01c6e8cfec1db2e75a19828d394
gerardparis <gerard.paris@gmail.com>
2016-07-15 12:53:43 +0200

    Tested common utils

---
+++ sds_controller/sds_controller/tests.py
@@ -0,0 +1,58 @@
+import redis
+
+from django.conf import settings
+from django.test import TestCase, override_settings
+
+from .common_utils import get_all_registered_nodes, remove_extra_whitespaces, to_json_bools
+
+# Tests use database=10 instead of 0.
+@override_settings(REDIS_CON_POOL=redis.ConnectionPool(host='localhost', port=6379, db=10))
+class MainTestCase(TestCase):
+    def setUp(self):
+        self.r = redis.Redis(connection_pool=settings.REDIS_CON_POOL)
+        self.create_nodes()
+
+    def tearDown(self):
+        self.r.flushdb()
+
+    def test_remove_extra_whitespaces_ok(self):
+        ret = remove_extra_whitespaces("a  b c   d e     f")
+        self.assertEqual(ret, "a b c d e f")
+
+        ret = remove_extra_whitespaces("      a  b c   d e     f  ")
+        self.assertEqual(ret, "a b c d e f")
+
+    def test_get_all_registered_nodes_ok(self):
+        node_list = get_all_registered_nodes()
+        self.assertEqual(len(node_list), 3)
+        sorted_list = sorted(node_list, key=lambda node: node['name'])
+        self.assertEqual(sorted_list[0]['name'], 'controller')
+        self.assertEqual(sorted_list[1]['name'], 'storagenode1')
+        self.assertEqual(sorted_list[2]['name'], 'storagenode2')
+
+    def test_to_json_bools_ok(self):
+        bdict = {'a':'True', 'b':'False', 'c':'True', 'd':'False'}
+        to_json_bools(bdict, 'a', 'b', 'c')
+        self.assertEqual(bdict['a'], True)
+        self.assertNotEqual(bdict['a'], 'True')
+        self.assertEqual(bdict['b'], False)
+        self.assertNotEqual(bdict['b'], 'False')
+        self.assertEqual(bdict['c'], True)
+        self.assertNotEqual(bdict['c'], 'True')
+        self.assertEqual(bdict['d'], 'False')
+        self.assertNotEqual(bdict['d'], False)
+
+    #
+    # Aux methods
+    #
+
+    def create_nodes(self):
+        self.r.hmset('node:controller',
+                     {'ip': '192.168.2.1', 'last_ping': '1467623304.332646', 'type': 'proxy', 'name': 'controller',
+                      'devices': '{"sdb1": {"free": 16832876544, "size": 16832880640}}'})
+        self.r.hmset('node:storagenode1',
+                     {'ip': '192.168.2.2', 'last_ping': '1467623304.332646', 'type': 'object', 'name': 'storagenode1',
+                      'devices': '{"sdb1": {"free": 16832876544, "size": 16832880640}}'})
+        self.r.hmset('node:storagenode2',
+                     {'ip': '192.168.2.3', 'last_ping': '1467623304.332646', 'type': 'object', 'name': 'storagenode2',
+                      'devices': '{"sdb1": {"free": 16832876544, "size": 16832880640}}'})
