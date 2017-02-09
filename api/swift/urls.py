f3ce259d92c7cbf69fcd412bf33e56fc495ca604
gerardparis <gerard.paris@gmail.com>
2017-02-09 10:39:30 +0100

    Fix bug in URLs

--- api/swift/urls.py
+++ api/swift/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^/spolicies/?$', views.storage_policies),
     url(r'^/locality/(?P<account>\w+)(?:/(?P<container>[-\w]+))(?:/(?P<swift_object>[-\w]+))?/$', views.locality_list),
     url(r'^/sort_nodes/?$', views.sort_list),
-    url(r'^/sort_nodes/(?P<id>[0-9]+)/?$', views.sort_detail),
+    url(r'^/sort_nodes/(?P<sort_id>[0-9]+)/?$', views.sort_detail),
 
     # Node status
     url(r'^/nodes/?$', views.node_list),
