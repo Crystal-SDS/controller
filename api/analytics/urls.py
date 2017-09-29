84dc506ebc4f9aed4bceb50981bd069dc8f371fa
gerardparis <gerard.paris@gmail.com>
2017-09-29 13:39:16 +0200

    Fixed issue with slashes in analytics URL

--- api/analytics/urls.py
+++ api/analytics/urls.py
@@ -4,11 +4,11 @@ import views
 
 urlpatterns = [
 
-    url(r'^/analyzers/?$', views.analyzer_list),
-    url(r'^/analyzers/data/?$', views.AnalyzerData.as_view()),
-    url(r'^/analyzers/(?P<analyzer_id>\w+)/data/?$', views.AnalyzerData.as_view()),
-    url(r'^/analyzers/(?P<analyzer_id>\d+)/?$', views.analyzer_detail),
-    url(r'^/jobs/?$', views.job_history_list),
-    url(r'^/jobs/data/?$', views.JobSubmitData.as_view()),
+    url(r'^analyzers/?$', views.analyzer_list),
+    url(r'^analyzers/data/?$', views.AnalyzerData.as_view()),
+    url(r'^analyzers/(?P<analyzer_id>\w+)/data/?$', views.AnalyzerData.as_view()),
+    url(r'^analyzers/(?P<analyzer_id>\d+)/?$', views.analyzer_detail),
+    url(r'^jobs/?$', views.job_history_list),
+    url(r'^jobs/data/?$', views.JobSubmitData.as_view()),
 
 ]
