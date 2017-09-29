327e0d7e2db18407a119a67296fd0109f80c9bf3
JosepSampe <jsampe@gmail.com>
2017-09-29 20:12:38 +0200

    Solved Merge conflict

--- api/api/urls.py
+++ api/api/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     url(r'^filters/', include('filters.urls')),
     url(r'^metrics/', include('metrics.urls')),
     url(r'^policies/', include('policies.urls')),
-<<<<<<< HEAD
-    url(r'^controller/', include('controller.urls')),
-    url(r'^analytics/', include('analytics.urls')),
-=======
     url(r'^controllers/', include('controllers.urls')),
->>>>>>> refs/heads/dev
+    url(r'^analytics/', include('analytics.urls')),
+
 ]
